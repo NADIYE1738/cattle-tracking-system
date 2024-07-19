1d000784cc4b02a8c0e3c30211cc7dfe8c964d28
NADIYE ENOSI <145094471+NADIYE1738@users.noreply.github.com>
2024-07-19 20:50:09 +0300

    Add files via upload

---
+++ sketch_jul19a.ino
@@ -0,0 +1,133 @@
+#include <Adafruit_MPU6050.h>
+#include <Adafruit_Sensor.h>
+#include <Wire.h>
+#include <dht.h>
+#include <SoftwareSerial.h>
+
+#define outPin 8
+
+Adafruit_MPU6050 mpu;
+dht DHT;
+
+//Create software serial object to communicate with SIM800L
+SoftwareSerial mySerial(3, 2); //SIM800L Tx & Rx is connected to Arduino #3 & #2
+
+void setup(void) {
+  Serial.begin(115200);
+
+  // Try to initialize!
+  if (!mpu.begin()) {
+    Serial.println("Failed to find MPU6050 chip");
+    while (1) {
+      delay(10);
+    }
+  }
+  Serial.println("MPU6050 Found!");
+
+  // set accelerometer range to +-8G
+  mpu.setAccelerometerRange(MPU6050_RANGE_8_G);
+
+  // set gyro range to +- 500 deg/s
+  mpu.setGyroRange(MPU6050_RANGE_500_DEG);
+
+  // set filter bandwidth to 21 Hz
+  mpu.setFilterBandwidth(MPU6050_BAND_21_HZ);
+
+  delay(100);
+
+  //Begin serial communication with Arduino and Arduino IDE (Serial Monitor)
+  Serial.begin(9600);
+  
+  //Begin serial communication with Arduino and SIM800L
+  mySerial.begin(9600);
+
+  Serial.println("Initializing...");
+  delay(1000);
+
+  mySerial.println("AT"); //Once the handshake test is successful, it will back to OK
+  updateSerial();
+  mySerial.println("AT+CSQ"); //Signal quality test, value range is 0-31 , 31 is the best
+  updateSerial();
+  mySerial.println("AT+CCID"); //Read SIM information to confirm whether the SIM is plugged
+  updateSerial();
+  mySerial.println("AT+CREG?"); //Check whether it has registered in the network
+  updateSerial();
+  mySerial.println("AT+CMGF=1"); // Configuring TEXT mode
+  updateSerial();
+  mySerial.println("AT+CMGS=\"+256771301999\"");//change ZZ with country code and xxxxxxxxxxx with phone number to sms
+  updateSerial();
+  mySerial.print("Jessy is ever single as a broke man | from oscarodongkara@gmail.com"); //text content
+  updateSerial();
+  mySerial.write(26);
+  updateSerial();
+  mySerial.println("AT+CMGF=1"); // Configuring TEXT mode
+  updateSerial();
+  mySerial.println("AT+CNMI=1,2,0,0,0"); // Decides how newly arrived SMS messages should be handled
+  updateSerial();
+  mySerial.println("ATD+ +256771301999;"); //  change ZZ with country code and xxxxxxxxxxx with phone number to dial
+  updateSerial();
+  delay(200000); // wait for 2 minutes...
+  mySerial.println("ATH"); //hang up
+  updateSerial();
+}
+
+void loop() {
+  /* Get new sensor events with the readings */
+  sensors_event_t a, g, temp;
+  mpu.getEvent(&a, &g, &temp);
+
+  /* Print out the values */
+  Serial.print("Acceleration X: ");
+  Serial.print(a.acceleration.x);
+  Serial.print(", Y: ");
+  Serial.print(a.acceleration.y);
+  Serial.print(", Z: ");
+  Serial.print(a.acceleration.z);
+  Serial.println(" m/s^2");
+
+  Serial.print("Rotation X: ");
+  Serial.print(g.gyro.x);
+  Serial.print(", Y: ");
+  Serial.print(g.gyro.y);
+  Serial.print(", Z: ");
+  Serial.print(g.gyro.z);
+  Serial.println(" rad/s");
+
+  Serial.print("Temperature: ");
+  Serial.print(temp.temperature);
+  Serial.println(" degC");
+
+  Serial.println("");
+
+  int readData = DHT.read11(outPin);
+
+  float t = DHT.temperature;        
+  float h = DHT.humidity;          
+
+  Serial.print("Temperature = ");
+  Serial.print(t);
+  Serial.print("°C | ");
+  Serial.print((t*9.0)/5.0+32.0);        
+  Serial.println("°F ");
+  Serial.print("Humidity = ");
+  Serial.print(h);
+  Serial.println("% ");
+  Serial.println("");
+
+  delay(2000); // wait two seconds
+
+  updateSerial();
+}
+
+void updateSerial()
+{
+  delay(500);
+  while (Serial.available()) 
+  {
+    mySerial.write(Serial.read());//Forward what Serial received to Software Serial Port
+  }
+  while(mySerial.available()) 
+  {
+    Serial.write(mySerial.read());//Forward what Software Serial received to Serial Port
+  }
+}
